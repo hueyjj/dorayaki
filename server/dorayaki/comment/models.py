61136da6c8ce830338bb85ca089b570231a05a3c
jasper <jasperjeng@gmail.com>
2019-09-22 20:12:06 -0700

    Add parent field to comment serializer

--- server/dorayaki/comment/models.py
+++ server/dorayaki/comment/models.py
@@ -8,6 +8,6 @@ from dorayaki.thread.models import Thread
 class Comment(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
     thread = models.ForeignKey(Thread, on_delete=models.PROTECT)
-    children = models.ForeignKey('self', on_delete=models.PROTECT, blank=True, null=True)
+    parent = models.ForeignKey('self', on_delete=models.PROTECT, blank=True, null=True)
     text = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
--- server/dorayaki/comment/serializers.py
+++ server/dorayaki/comment/serializers.py
@@ -3,14 +3,13 @@ from rest_framework import serializers
 from dorayaki.comment.models import Comment
 
 # https://stackoverflow.com/questions/13376894/django-rest-framework-nested-self-referential-objects#answer-27236783
-class RecursiveSerializer(serializers.Serializer):
+class RecursiveCommentSerializer(serializers.Serializer):
     def to_representation(self, value):
-        serializer = self.parent.parent.__class__(value, context=self.context)
+        serializer = CommentSerializer(value, context=self.context)
         return serializer.data
 
 class CommentSerializer(serializers.ModelSerializer):
-    #parent = RecursiveSerializer(many=True)
-    children = RecursiveSerializer(source="comment_set", many=True)
+    replies = RecursiveCommentSerializer(source="comment_set", many=True)
     class Meta:
         model = Comment
-        fields = ['pk', 'owner', 'thread', 'children', 'text', 'created']
+        fields = ['pk', 'owner', 'thread', 'parent', 'text', 'created', 'replies']
