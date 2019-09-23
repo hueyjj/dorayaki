071038d5ef86914a23eb932a5daeff58aff3605d
jasper <jasperjeng@gmail.com>
2019-09-23 08:20:48 -0700

    Do not require replies to create a new comment

--- server/dorayaki/comment/serializers.py
+++ server/dorayaki/comment/serializers.py
@@ -2,14 +2,14 @@ from rest_framework import serializers
 
 from dorayaki.comment.models import Comment
 
-# https://stackoverflow.com/questions/13376894/django-rest-framework-nested-self-referential-objects#answer-27236783
+# Modified version from https://stackoverflow.com/questions/13376894/django-rest-framework-nested-self-referential-objects#answer-27236783
 class RecursiveCommentSerializer(serializers.Serializer):
     def to_representation(self, value):
         serializer = CommentSerializer(value, context=self.context)
         return serializer.data
 
 class CommentSerializer(serializers.ModelSerializer):
-    replies = RecursiveCommentSerializer(source="comment_set", many=True)
+    replies = RecursiveCommentSerializer(source="comment_set", required=False, many=True)
     class Meta:
         model = Comment
         fields = ['pk', 'owner', 'thread', 'parent', 'text', 'created', 'replies']
